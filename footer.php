f4699514ff9c59ee114489c2a0e4ff17ea43063d
Brannon Dorsey <brannon@brannondorsey.com>
2014-02-25 18:45:46 -0600

    Banner images now transition. Added home link in menu.

--- footer.php
+++
@@ -1,7 +0,0 @@
-	  <footer>
-	    <span><?php echo kirbytext($site->footer_text()) ?></span>
-	    <span id="copyright"><?php echo kirbytext($site->copyright())?></span>
-	  </footer>
-
-	</body>
-</html>
