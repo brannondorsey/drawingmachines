e787134c66d8080a79beb625cae732117fa56a11
Brannon Dorsey <brannon@brannondorsey.com>
2015-01-02 15:24:43 -0500

    File renames and minor menu work

--- includes/menu.php
+++ includes/menu.php
@@ -2,19 +2,18 @@
     
     <div id="header-top-box">
 	    <h1><a href="index.php">DRAWINGMACHINES.ORG</a></h1>
-		<nav class="menu">
-		  <ul>
-		  	<li><a href="index.php">Home</a></li>
-		    <li><a href="categories.php">Categories</a></li>
-		    <li><a href="about.php">About/FAQ</a></li>
-		  </ul>
-		</nav>
 	</div>
 	<div id="header-bottom-box">
 		<span id="description"><?php echo $site_description; ?></span>
-		<form class="search" method="GET" action="results.php">
+		<nav class="menu">
+		  <ul>
+		    <li><a href="index.php">Home</a></li>
+		    <li><a href="about.php">About/FAQ</a></li>
+		  </ul>
+		</nav>
+		<!--<form class="search" method="GET" action="results.php">
 			<input type="text" value="" placeholder="Enter keyword, e.g. Prism"></input>
 			<input type="submit" value="Search"></input>
-		</form>
+		</form>-->
 	</div>
 </header>
--- index.php
+++ index.php
@@ -1,49 +1,85 @@
-<?php require_once "includes/header.php" ?>
-<?php require_once "includes/menu.php";
+<?php 
+	require_once "includes/classes/class.API.inc.php";
+	require_once "includes/config.php";
+
+	$category_columns = 
+				"id,
+  				category,
+  				class,
+  				description";
+
+  	//setup the API
+  	$category_api = new API("localhost", 
+  				   $DATABASE, 
+  				   "categories", 
+  				   $DATABASE_USER, 
+  				   $DATABASE_PASSWORD);
+
+  	$category_api->setup($category_columns);
+  	$category_api->set_default_order("class");
+
+  	$query_array = array('limit' => 50);
+
+	$resultsObj = json_decode($category_api->get_json_from_assoc($query_array));
 	
-	//get banner images
-	$banner_image_dir = "images/banners";
-	$banner_images = scandir($banner_image_dir);
-	for ($i = 0; $i < count($banner_images); $i++) {
-		if (strpos($banner_images[$i], ".png") === false) unset($banner_images[$i]);
-	}
-	shuffle($banner_images);
-	
-?>
+	$categories = array();
+	$class_names = array();
 
-<div class="content">
-	<script>
+	if (isset($resultsObj->data[0])) {
 
-		$(document).ready(function(){
-			// Run our swapImages() function every 5secs
-			setInterval('swapImages()', 5000);
-
-			$('#banner-gallery img').click(function(){
-				window.location.href = 'post.php?id=' + $(this).attr('data-id');
-			});
-		});
-
-		function swapImages(){
-
-		  var active = $('#banner-gallery .active');
-		  var next = ($('#banner-gallery .active').next().length > 0) 
-		  		? $('#banner-gallery .active').next() : $('#banner-gallery img:first');
+		$tmp = $resultsObj->data;
 		
-		  active.fadeOut(650, function(){
-		    active.removeClass('active');
-		    next.fadeIn().addClass('active');
-		  });
+		for ($i = 0; $i < count($tmp); $i++) {
+			array_push($class_names, $tmp[$i]->class);
 		}
 
-	</script>
-	<div id="banner-gallery">
-		<?php for ($i = 0; $i < count($banner_images); $i++):?>
-			<img src="<?php echo $banner_image_dir . "/" . $banner_images[$i]?>" 
-			<?php if ($i == 0) echo "class=\"active\"" ?> 
-			data-id="<?php echo (int) str_replace(".png", "", $banner_images[$i])?>"/>
-		<?php endfor?>
+		$class_names = array_unique($class_names);
+		$class_names = array_values($class_names);
+
+		for ($i = 0; $i < count($class_names); $i++) {
+			$categories[$class_names[$i]] = array();
+		}
+
+		for ($i = 0; $i < count($tmp); $i++) {
+			$category = $tmp[$i];
+			array_push($categories[$category->class], $category);
+		}
+	}
+
+	require_once "includes/header.php";
+	require_once "includes/menu.php";
+?>
+
+<script>
+	$(document).ready(function(){
+		$('.category-box').on('mouseout mouseover', function(evt){
+			// $(this).find('.category-name').toggleClass('hidden');
+		});
+	});
+</script>
+
+<div class="content categories-layout">
+	<h3 style="margin-top: 0;">Browse by category</h3>
+	<?php 
+	for($i = 0; $i < count($class_names); $i++): 
+		$class = $class_names[$i];?>
+	<div class="category-class">
+		<p class="category-class-name"><?php echo $class?></p>
+		<?php
+		for ($j = 0; $j < count($categories[$class]); $j++):
+			$categoryObj = $categories[$class][$j];
+			$image_safe_name = utf8_encode($categoryObj->category);
+			$image_safe_name = str_replace("/", "-", $categoryObj->category);
+			$image_file = "images/category/thumbnail/" . $image_safe_name . " Thumb.png"?>
+		<a href="category.php?id=<?php echo $categoryObj->id?>">
+			<div class="category-box">
+				<div class="category-name">&nbsp<?php echo $categoryObj->category ?></div>
+				<img src="<?php echo $image_file?>">
+			</div>
+		</a>
+	<?php endfor; ?>
 	</div>
-	<p>Looking for a drawing machine? Search by function or browse by category <a href="find.php">here</a>.</p>
+<?php endfor; ?>
 </div>
 
 <?php require_once "includes/footer.php" ?>
