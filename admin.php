4053fbafa1e2093115cc1b32a78c8740aa0341ec
Brannon Dorsey <brannon@brannondorsey.com>
2015-01-04 19:01:29 -0500

    remove http link

--- admin.php
+++ admin.php
@@ -248,7 +248,6 @@
 ?>
 
 <link rel="stylesheet" type="text/css" href="styles/autosuggest.css">
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" ></script>
 <script type="text/javascript" src="scripts/jquery.autosuggest.minified.js"></script>
 <script>
 	var hostname = <?php echo '"' . $HOSTNAME . '"'; ?>;
