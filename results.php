7ef96c0ba6f19d64fbfe4eed34fc77aee9414e28
Brannon Dorsey <brannon@brannondorsey.com>
2015-01-18 22:22:11 -0600

    Fix page count bug on results page

--- results.php
+++ results.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 	 require_once "includes/database_connect.php";
 	 require_once "includes/helpers.php";
@@ -12,12 +12,12 @@
 	 	$page = (isset($_GET['page']) ? $_GET['page'] : 1);
 
 	 	if (isset($results_obj->data)) {
-	 		
-	 		$limit = (isset($query_array['limit'])) ? (int) $query_array : 0;
+
+	 		$limit = (isset($query_array['limit'])) ? (int) $query_array['limit'] : 0;
 	 		$numb_results = max(count($results_obj->data), $limit);
 	 		$total_numb_results = total_numb_results($query_array, $api); //gives total number of pages
-		    $total_pages = ceil($total_numb_results / $numb_results); //calculates total number of pages
-		    if ($page > $total_pages) $page = $total_pages; //sets page to max page if page it exceeds it
+		  $total_pages = ceil($total_numb_results / $numb_results); //calculates total number of pages
+		  if ($page > $total_pages) $page = $total_pages; //sets page to max page if page it exceeds it
 
 	 	} else {
 	 		//error or no results found
@@ -27,7 +27,7 @@
 
 	 require_once "includes/header.php";
 	 require_once "includes/menu.php";
-	 require_once "includes/helpers.php"; 
+	 require_once "includes/helpers.php";
 ?>
 <script>
 	$(document).ready(function(){
@@ -38,14 +38,14 @@
 </script>
 
 <div class="content results-container">
-	<h2>Showing results for "<?php 
+	<h2>Showing results for "<?php
 		if (isset($query_array["tags"])) echo $query_array["tags"];
 		else if (isset($query_array["category"])) echo $query_array["category"];
 		else if (isset($query_array["search"])) echo $query_array["search"];
 	?>"</h2>
-	<?php 
+	<?php
 	if (isset($results_obj->data)) :
-		foreach($results_obj->data as $machine): 
+		foreach($results_obj->data as $machine):
 	?>
 	<div class="result" data-id="<?php echo $machine->id?>">
 		<img src="<?php echo get_machine_thumbnail($machine)?>">
@@ -60,7 +60,7 @@
     <div class="pagination-container">
         <?php if ($page > 1 &&
         		  isset($query_array)) {
-        	$query_array["page"] = $page - 1; 
+        	$query_array["page"] = $page - 1;
         	$url_parameters = http_build_query($query_array);
         	?>
         <a class="prev" href="results.php?<?php echo $url_parameters; ?>">&lt;</a>
@@ -68,7 +68,8 @@
         <span class="count"><?php echo min($page, $total_pages) . " of " . $total_pages ?></span>
         <?php if ($page < $total_pages &&
         		  isset($query_array)) {
-        	$query_array["page"] = $page + 1; 
+
+        	$query_array["page"] = $page + 1;
         	$url_parameters = http_build_query($query_array);?>
         <a class="next" href="results.php?<?php echo $url_parameters; ?>">&gt;</a>
         <?php } ?>
