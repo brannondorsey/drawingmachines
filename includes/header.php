da977062642ec5118d026e6d3f9d88e4e4174206
Brannon Dorsey <brannon@brannondorsey.com>
2015-01-22 14:55:22 -0600

    Add favicon

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8" />
     <meta name="description" content="<?php echo $site_description; ?>" />
     <meta name="robots" content="index, follow" />
-
+    <link rel="shortcut icon" type="image/ico" href="favicon.ico"/>
     <link rel="stylesheet" type="text/css" href="styles/base.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
 
